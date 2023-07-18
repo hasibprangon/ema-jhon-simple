49bd0190833e9fd36d9ffdb13cb71c20ae3a3376
hasibprangon <hasibulhossainp09@gmail.com>
2023-07-19 02:27:44 +0600

    header and nav edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import './Header.css'
+import logo from '../../images/Logo.svg'
 const Header = () => {
     return (
-        <div>
-            <h2>this is header</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div className='others'>
+                <a href="/shop">shop</a>
+                <a href="/order">order</a>
+                <a href="/inventory">inventory</a>
+                <a href="/log in">Log in</a>
+            </div>
+        </nav>
     );
 };
 
